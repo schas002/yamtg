3703e4873b1346c287d870c2d16b5c25f485510b
Andrew Zyabin [🔒] <schas002@users.noreply.github.com>
2017-08-21 21:37:19 +0400

    :sparkles: add oak leaves
    No quality open art for leaves? Aww...

--- mods/default/nodes/wood.lua
+++ mods/default/nodes/wood.lua
@@ -6,13 +6,41 @@ minetest.register_node("default:tree_oak", {
 		"default_tree_oak.png",
 		"default_tree_oak.png",
 		"default_tree_oak.png",
-		"default_tree_oak.png",
+		"default_tree_oak.png"
 	},
 	groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 1},
 	is_ground_content = true
 })
 
-minetest.register_alias("default:tree", "default:tree_oak")
+minetest.register_node("default:leaves_oak", {
+	description = "Oak Leaves",
+	drawtype = "allfaces_optional",
+	tiles = {
+		"default_leaves_oak.png",
+		"default_leaves_oak.png",
+		"default_leaves_oak.png",
+		"default_leaves_oak.png",
+		"default_leaves_oak.png",
+		"default_leaves_oak.png"
+	},
+	paramtype = "light",
+	groups = {snappy = 3},
+	--[[ drop = {
+		items = {
+			{
+				-- player will get sapling with 1/20 chance
+				items = {'default:sapling_oak'},
+				rarity = 20
+			}
+		}
+	} ]]-- TODO implement saplings
+	waving = 2
+})
 
+minetest.register_alias("default:tree", "default:tree_oak")
 minetest.register_alias("mapgen_tree", "default:tree_oak")
 minetest.register_alias("tree", "default:tree_oak")
+
+minetest.register_alias("default:leaves", "default:leaves_oak")
+minetest.register_alias("mapgen_leaves", "default:leaves_oak")
+minetest.register_alias("leaves", "default:leaves")
